ddb7cccb47eab8d86037f9ee68069960ffbeccc4
egfesser <egfesser@gmail.com>
2014-12-07 16:33:10 -0600

    Added additional comments

--- plot1.R
+++ plot1.R
@@ -1,8 +1,10 @@
-## Run as script.
+## Run as script
 
 fileUrl <- ("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")
 fileZip <- "exdata_data_household_power_consumption.zip"
 fileTxt <- "household_power_consumption.txt"
+
+# Get file
 
 setAs("character","myDate",function(from) as.Date(from, format="%d/%m/%Y"))
 setClass("myDate") 
@@ -12,18 +14,18 @@ if(!file.exists(fileZip)) {
         download.file(fileUrl, fileDest)
 }
 
+# Unzip and read file
+
 unzip(fileZip, exdir=".")
 fileData <- read.table(fileTxt, sep=";", header=TRUE, 
         na.strings="?", colClasses=colClasses, comment.char = "",nrows=2075259)
 
+# Filter data for 2 dates to be interrogated
+
 dates <- c(as.Date("1/2/2007", format="%d/%m/%Y"),as.Date("2/2/2007", format="%d/%m/%Y"))
-
 filteredData <- subset(fileData,fileData$Date %in% dates)
-filteredData2 <- subset(filteredData,filteredData$Global_active_power != "?")
 
-gooddata <- data.frame(as.POSIXct(paste(filteredData2$Date,filteredData2$Time,sep=" ")),filteredData2$Global_active_power)
-cnames <- c("datetime","data")
-colnames(gooddata) <- cnames
+# Plot
 
 png(file="plot1.png", height=480, width=480)
 hist(filteredData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") ##, ylab="Frequency", axes=FALSE)
--- plot3.R
+++ plot3.R
@@ -1,8 +1,10 @@
-## Run as script.
+## Run as script
 
 fileUrl <- ("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")
 fileZip <- "exdata_data_household_power_consumption.zip"
 fileTxt <- "household_power_consumption.txt"
+
+# Get file
 
 setAs("character","myDate",function(from) as.Date(from, format="%d/%m/%Y"))
 setClass("myDate") 
@@ -12,24 +14,27 @@ if(!file.exists(fileZip)) {
         download.file(fileUrl, fileDest)
 }
 
+# Unzip and read file
+
 unzip(fileZip, exdir=".")
 fileData <- read.table(fileTxt, sep=";", header=TRUE, 
         na.strings="?", colClasses=colClasses, comment.char = "",nrows=2075259)
 
-dates <- c(as.Date("1/2/2007", format="%d/%m/%Y"),as.Date("2/2/2007", format="%d/%m/%Y"))
+# Filter data for 2 dates to be interrogated
 
+dates <- c(as.Date("1/2/2007", format="%d/%m/%Y"),as.Date("2/2/2007", format="%d/%m/%Y"))
 filteredData <- subset(fileData,fileData$Date %in% dates)
 filteredData3 <- subset(filteredData,filteredData$Sub_metering_1 != "?")
 filteredData4 <- subset(filteredData,filteredData$Sub_metering_2 != "?")
 filteredData5 <- subset(filteredData,filteredData$Sub_metering_3 != "?")
-
 gooddata2 <- data.frame(as.POSIXct(paste(filteredData3$Date,filteredData3$Time,sep=" ")),filteredData3$Sub_metering_1)
 gooddata3 <- data.frame(as.POSIXct(paste(filteredData4$Date,filteredData4$Time,sep=" ")),filteredData4$Sub_metering_2)
 gooddata4 <- data.frame(as.POSIXct(paste(filteredData5$Date,filteredData5$Time,sep=" ")),filteredData5$Sub_metering_3)
-
 colnames(gooddata2) <- cnames
 colnames(gooddata3) <- cnames
 colnames(gooddata4) <- cnames
+
+# Plot
 
 png(file="plot3.png", height=480, width=480)
 plot(gooddata2$datetime,gooddata2$data,xlab="",ylab="Energy sub metering", type="l", col="black")
